96dead9c48108e29379748b6ef2b49288c42d11a
Dima Parzhitsky <parzhitsky@gmail.com>
2020-01-27 22:59:32 +0200

    Naming changes

--- src/.snapshot-resolver.js
+++ src/.snapshot-resolver.js
@@ -1,20 +1,20 @@
 module.exports = {
 	/**
-	 * @param {string} testPath Path to test file, relative to the current file (e.g., "components/article.spec.tsx")
-	 * @param {string} snapshotExtension Snapshot extension, including leading dot (e.g., ".snap")
+	 * @param {string} testFilePath Path to test file, relative to the current file (e.g., "components/article.spec.tsx")
+	 * @param {string} snapExtension Snapshot extension, including leading dot (e.g., ".snap")
 	 * @returns {string}
 	 */
-	resolveSnapshotPath(testPath, snapshotExtension) {
-		return testPath.replace(".spec", `${ snapshotExtension }.spec`);
+	resolveSnapshotPath(testFilePath, snapExtension) {
+		return testFilePath.replace(".spec", `${ snapExtension }.spec`);
 	},
 
 	/**
-	 * @param {string} snapPath Path to snapshot file, relative to the current file (e.g., "components/article.snap.spec.tsx")
-	 * @param {string} snapshotExtension Snapshot extension, including leading dot (e.g., ".snap")
+	 * @param {string} snapFilePath Path to snapshot file, relative to the current file (e.g., "components/article.snap.spec.tsx")
+	 * @param {string} snapExtension Snapshot extension, including leading dot (e.g., ".snap")
 	 * @returns {string}
 	 */
-	resolveTestPath(snapPath, snapshotExtension) {
-		return snapPath.replace(snapshotExtension, "");
+	resolveTestPath(snapFilePath, snapExtension) {
+		return snapFilePath.replace(snapExtension, "");
 	},
 
 	testPathForConsistencyCheck: "components/article.tsx",
